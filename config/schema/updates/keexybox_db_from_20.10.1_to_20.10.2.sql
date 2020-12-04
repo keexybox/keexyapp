6fabaef44c7f53bcdd9102c6dd32d72670ae8827
Benoit <benoit.saglietto@gmail.com>
2020-12-04 08:24:23 +0000

    Add usefull tool and links for user into connection view

---
+++ config/schema/updates/keexybox_db_from_20.10.1_to_20.10.2.sql
@@ -0,0 +1,5 @@
+UPDATE config SET value='20.10.2' WHERE param='version';
+INSERT INTO `config` VALUES ('cportal_homepage_button_url', 'https://duckduckgo.com/', 'setting', 'Home page button URL to the Internet');
+INSERT INTO `config` VALUES ('cportal_homepage_button_name', 'Browse the Internet', 'setting', 'Home page button name to the Internet');
+INSERT INTO `config` VALUES ('cportal_check_tor_url', 'https://check.torproject.org', 'setting', 'URL to check if a user use Tor');
+INSERT INTO `config` VALUES ('cportal_ip_info_url', 'http://ifconfig.co/', 'setting', 'URL to check Internet IP address and Finger print');
