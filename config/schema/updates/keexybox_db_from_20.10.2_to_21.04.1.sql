b25b251b4cab9f580fd136ae1c65b5451382318e
Benoit <benoit.saglietto@gmail.com>
2021-04-05 06:50:50 +0100

    Working on new DHCP configuration

--- config/schema/updates/keexybox_db_from_20.10.2_to_21.04.1.sql
+++ config/schema/updates/keexybox_db_from_20.10.2_to_21.04.1.sql
@@ -1,3 +1,5 @@
 UPDATE config SET value='20.10.2' WHERE param='version';
 ALTER TABLE `config` MODIFY description VARCHAR(300);
 UPDATE config SET description='Bahavior of the Captive Portal - 0=Private (admin need to create accounts for users), 1=Registration (The user can create his account himself), 2=Free (Free access by accepting terms and conditions), 3=None (Captive portal is not needed to access the Internet)' WHERE param='cportal_register_allowed';
+INSERT INTO `config` VALUES ('dhcp_enabled_input', '0', 'setting', 'Enable/Disable DHCP for input network. set 1 to enable.');
+INSERT INTO `config` VALUES ('dhcp_enabled_output', '0', 'setting', 'Enable/Disable DHCP for output network. set 1 to enable.');
